8fb5663d0e966162157d08b0b90822fe0a3af730
Sheshank Jamwal <sheshank.jamwal@debugged-pro.com>
2023-02-20 19:46:02 +0530

    mobile responsive

--- pages/components/header.js
+++ pages/components/header.js
@@ -4,9 +4,20 @@ import styles from '@/styles/Home.module.css'
 export default function Header() {
 return (
 <>
+
+
 <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.innerheader}>
+        <div className='menuhamburger'>
+          <div className='burgericon'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2 7L22 7" stroke="black" stroke-width="1.4" stroke-linecap="square"/>
+            <path d="M2 12.5L16 12.5" stroke="black" stroke-width="1.4" stroke-linecap="square"/>
+            <path d="M2 18L8 18" stroke="black" stroke-width="1.4" stroke-linecap="square"/>
+            </svg>
+          </div>
+        </div>
           <div className={styles.leftsection}>
             <div className={styles.logosection}>
               <h1><Link href="/">GlobalTalk</Link></h1>
@@ -48,6 +59,9 @@ return (
           </div>
         </div>
       </div>
+
+
+
     </header>
 </>
 )
